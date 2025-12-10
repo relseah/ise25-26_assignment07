da50df14dd1930fcba9cbac04d54c1be81d7c471
David Häsler <david.haesler@proton.me>
2025-12-10 11:04:38 +0100

    Implement ReviewDto

--- api/src/main/java/de/seuhd/campuscoffee/api/dtos/ReviewDto.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/dtos/ReviewDto.java
@@ -1,6 +1,11 @@
 package de.seuhd.campuscoffee.api.dtos;
 
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import lombok.NonNull;
 import org.jspecify.annotations.Nullable;
 
 /**
@@ -8,8 +13,18 @@ import org.jspecify.annotations.Nullable;
  */
 @Builder(toBuilder = true)
 public record ReviewDto (
-    @Nullable Long id
-    // TODO: Implement ReviewDto
+    @Nullable Long id,
+
+    @Nullable LocalDateTime createdAt,
+    @Nullable LocalDateTime updatedAt,
+
+    @NotNull
+    @NonNull Long posId,
+    @NotNull
+    @NonNull Long authorId,
+    @NotBlank
+    @NonNull String review,
+    @Nullable Boolean approved
 ) implements Dto<Long> {
     @Override
     public @Nullable Long getId() {
